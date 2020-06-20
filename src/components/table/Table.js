944e31eae3ee4c388c8a22be27baa12eea73f231
nikay666 <lilit666an@gmail.com>
2020-06-20 04:35:29 +0300

    finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,23 @@
 import { ExcelComponents } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
-
+import { resizeHadler } from './table.resize'
+import { shouldResize } from './table.functions';
+ 
 export class Table extends ExcelComponents{
+
+    constructor($root){
+        super($root, {
+            listeners: ['mousedown']
+        })
+    }
+
     static className = 'excel__table';
     toHTML(){
         return createTable(35);
-
+    }
+    onMousedown(event){
+        if(shouldResize(event)){
+            resizeHadler(this.$root, event);
+        }
     }
 }
