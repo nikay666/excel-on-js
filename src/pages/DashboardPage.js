c65bea203b465092ced7b7ada90426f40bd5de27
nikay666 <lilit666an@gmail.com>
2020-07-04 23:33:16 +0300

    implement DIP

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,6 +1,6 @@
-import { Page } from "../core/Page";
+import { Page } from "../core/page/Page";
 import { $ } from "../core/dom";
-import { toHTML, createRecordsTable } from "./dashboard.functions";
+import { toHTML, createRecordsTable } from "../shared/dashboard.functions";
 
 export class DashboardPage extends Page{
     getRoot(){
